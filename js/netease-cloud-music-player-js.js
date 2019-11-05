6efdc139726b8b8671f62c0ef46cc970903478c7
jianghaowen <jianghaowen@webmail.hzau.edu.cn>
2019-11-05 16:29:49 +0800

    Update netease-cloud-music-player-js.js

--- js/netease-cloud-music-player-js.js
+++ js/netease-cloud-music-player-js.js
@@ -1,20 +1,16 @@
- jQuery(document).ready(function () {
- jQuery("#music5").click(
-    function(){
-               jQuery("#music4").css('display','block');
-               jQuery("#music5").css('display','none');
-			   jQuery("#guan2").addClass('open');
-                           jQuery("#guan2").removeClass('close');
-    }
-);
- jQuery("#guan2").click(
-    function(){
-               jQuery("#music4").css('display','none');
-               jQuery("#music5").css('display','block');
-			   jQuery("#guan2").removeClass('open');
-                           jQuery("#guan2").addClass('close');
-    }
-);
+jQuery(document).ready(function () { 
+jQuery("#music5").click(    
+	function(){               
+	           jQuery("#music4").css('display','block');               
+			   jQuery("#music5").css('display','none');			   
+			   jQuery("#guan2").css('display','block');                             
+			   }); 
+jQuery("#guan2").click(    
+	function(){               
+	          jQuery("#music4").css('display','none');               
+			  jQuery("#music5").css('display','block');			   
+			  jQuery("#guan2").css('display','none');                             
+			  });
 });
 
 
